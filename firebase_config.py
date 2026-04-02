0f5d8396f3c92281938f569c559e12fcb35c0b93
Guduri Prateet Sai <160823303+PRSDesigns@users.noreply.github.com>
2026-04-02 18:40:07 +0530

    Add files via upload

---
+++ firebase_config.py
@@ -0,0 +1,10 @@
+import firebase_admin
+from firebase_admin import credentials, firestore, auth
+
+# Directly load JSON file
+cred = credentials.Certificate("../firebase-key.json")
+
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+firebase_auth = auth
